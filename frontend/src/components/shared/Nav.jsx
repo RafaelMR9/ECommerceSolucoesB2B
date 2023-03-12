8649c3c957c0d5744788b42e78645be76d5e3382
RafaelMR9 <rafaelmedeirosa98@gmail.com>
2023-03-12 16:46:19 -0300

    mock initial

--- frontend/src/components/shared/Nav.jsx
+++ frontend/src/components/shared/Nav.jsx
@@ -39,7 +39,7 @@ export default function Nav() {
         </div>
       </div>
       <div className="max-w-7xl mx-auto">
-        <div className="flex items-center justify-center h-16">
+        <div className="flex items-center justify-center h-12 pb-3">
           <Link href="/" className="text-gray-300 hover:text-white px-3 py-2 rounded-md text-sm font-medium">
             Produtos
           </Link>
