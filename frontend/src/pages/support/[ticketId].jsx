d9cdcf0e9371cda9a436b1437d125fafbe759236
RafaelMR9 <rafaelmedeirosa98@gmail.com>
2023-12-23 16:41:56 -0300

    loading screen support

--- frontend/src/pages/support/[ticketId].jsx
+++ frontend/src/pages/support/[ticketId].jsx
@@ -17,10 +17,13 @@ export default function AnswerTicket() {
   const [responses, setResponses] = useState([])
   const [newResponse, setNewResponse] = useState("")
   const [errorMessage, setErrorMessage] = useState("")
+  const [isLoading, setIsLoading] = useState(true)
 
   useEffect(() => {
     const fetchTicket = async () => {
       try {
+        setIsLoading(true)
+
         const ticket = await getTicket(ticketId)
         const responses = await getTicketResponses(ticketId)
         const administrator = await getAdministrator()
@@ -29,11 +32,14 @@ export default function AnswerTicket() {
         setResponses(responses)
         setTicket(ticket)
         setAdministrador(administrator)
+        setIsLoading(false)
+
       } catch (e) {
+        setIsLoading(false)
         router.push("/support")
       }
     }
-    
+
     fetchTicket()
   }, [])
 
@@ -71,69 +77,78 @@ export default function AnswerTicket() {
   return (
     <ProtectedRoute isProtected>
       <BaseLayout>
-        <h1 className="text-4xl font-bold text-slate-800 text-center lg:text-start mb-8">Suporte - Conversa</h1>
-        <div className="bg-white shadow overflow-hidden rounded-lg">
-          <div className="px-4 py-5">
-            <h3 className="text-lg leading-6 font-medium text-gray-900">
-              {ticket.subject}
-            </h3>
-            <p className="mt-1 max-w-2xl text-md text-gray-500">
-              {new Date(ticket.dateHour).toLocaleString()}
-            </p>
+        {isLoading ?
+          <div className="flex items-center justify-center mt-32">
+            <div className={`animate-spin ease-linear rounded-full h-32 w-32 pt-14 border-8 border-l-gray-400 border-r-gray-400 border-b-gray-400 border-t-indigo-900`} />
           </div>
-          <div className="border-t border-gray-200 px-4 py-5">
-            <dl className="sm:divide-y sm:divide-gray-200">
-            {responses.map((response, idx) => { 
-              return (
-              <div
-                key={response.id}
-                className={`px-4 py-5 sm:grid sm:grid-cols-3 ${idx % 2 === 0 ? "bg-gray-100" : ""
-                  }`}
-              >
-                <dt className="text-md font-medium text-gray-700">
-                  {response.sender === user.id ? "Você" : administrador.id === response.sender ? "Administrador" : "Organização Compradora"}
-                </dt>
-                <p className="text-sm text-gray-600">
-                  {new Date(response.dateHour).toLocaleString()}
+          :
+          <>
+            <h1 className="text-4xl font-bold text-slate-800 text-center lg:text-start mb-8">Suporte - Conversa</h1>
+            <div className="bg-white shadow overflow-hidden rounded-lg">
+              <div className="px-4 py-5">
+                <h3 className="text-lg leading-6 font-medium text-gray-900">
+                  {ticket.subject}
+                </h3>
+                <p className="mt-1 max-w-2xl text-md text-gray-500">
+                  {new Date(ticket.dateHour).toLocaleString()}
                 </p>
-                <dd className="mt-1 text-sm text-gray-900">
-                  {response.content}
-                </dd>
               </div>
-            )})}
-            </dl>
-          </div>
-        </div>
-        <form onSubmit={handleSubmit} className="mt-4">
-          <div className="mb-2">
-            <label className="block text-gray-700 font-bold mb-2" htmlFor="newResponse">
-              Resposta
-            </label>
-            <textarea
-              rows={4}
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-              id="newResponse"
-              name="newResponse"
-              value={newResponse}
-              onChange={handleChange}
-              placeholder="Digite sua Resposta..."
-              required
-            ></textarea>
-          </div>
-          {errorMessage && <p className="mb-2 text-red-600">{errorMessage}</p>}
-          <button
-            className="w-full lg:w-auto px-6 py-3 rounded-md shadow-sm font-medium text-white bg-blue-600 hover:bg-blue-700"
-            type="submit"
-          >
-            Enviar
-          </button>
-        </form>
-        <hr className="mt-6 border border-gray-400" />
-        <div className="mt-8 text-center">
-          <p className="text-gray-700">
-            Não quer Responder ou Resolvemos sua Questão? <Link href="/support" className="text-blue-600">Voltar para a Página de Suporte</Link>.
-          </p>
-        </div>
+              <div className="border-t border-gray-200 px-4 py-5">
+                <dl className="sm:divide-y sm:divide-gray-200">
+                  {responses.map((response, idx) => {
+                    return (
+                      <div
+                        key={response.id}
+                        className={`px-4 py-5 sm:grid sm:grid-cols-3 ${idx % 2 === 0 ? "bg-gray-100" : ""
+                          }`}
+                      >
+                        <dt className="text-md font-medium text-gray-700">
+                          {response.sender === user.id ? "Você" : administrador.id === response.sender ? "Administrador" : "Organização Compradora"}
+                        </dt>
+                        <p className="text-sm text-gray-600">
+                          {new Date(response.dateHour).toLocaleString()}
+                        </p>
+                        <dd className="mt-1 text-sm text-gray-900">
+                          {response.content}
+                        </dd>
+                      </div>
+                    )
+                  })}
+                </dl>
+              </div>
+            </div>
+            <form onSubmit={handleSubmit} className="mt-4">
+              <div className="mb-2">
+                <label className="block text-gray-700 font-bold mb-2" htmlFor="newResponse">
+                  Resposta
+                </label>
+                <textarea
+                  rows={4}
+                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  id="newResponse"
+                  name="newResponse"
+                  value={newResponse}
+                  onChange={handleChange}
+                  placeholder="Digite sua Resposta..."
+                  required
+                ></textarea>
+              </div>
+              {errorMessage && <p className="mb-2 text-red-600">{errorMessage}</p>}
+              <button
+                className="w-full lg:w-auto px-6 py-3 rounded-md shadow-sm font-medium text-white bg-blue-600 hover:bg-blue-700"
+                type="submit"
+              >
+                Enviar
+              </button>
+            </form>
+            <hr className="mt-6 border border-gray-400" />
+            <div className="mt-8 text-center">
+              <p className="text-gray-700">
+                Não quer Responder ou Resolvemos sua Questão? <Link href="/support" className="text-blue-600">Voltar para a Página de Suporte</Link>.
+              </p>
+            </div>
+          </>
+        }
       </BaseLayout>
     </ProtectedRoute>
   )
--- frontend/src/pages/support/index.jsx
+++ frontend/src/pages/support/index.jsx
@@ -9,53 +9,67 @@ export default function Support() {
 
   const { user } = useContext(AuthContext)
   const [tickets, setTickets] = useState([])
+  const [isLoading, setIsLoading] = useState(true)
 
   useEffect(() => {
     const fetchTickets = async () => {
       try {
+        setIsLoading(true)
+
         let tickets = []
         if (user.is_superuser)
-        tickets = await getTickets()
+          tickets = await getTickets()
         else
           tickets = await getUserTickets(user.id)
 
         setTickets(tickets)
+        setIsLoading(false)
+
       } catch (e) {
+        setIsLoading(false)
         alert(e.message)
       }
     }
-    
+
     fetchTickets()
   }, [])
 
   return (
     <ProtectedRoute isProtected>
       <BaseLayout>
-        <div className="flex justify-between mb-8">
-          <h1 className="text-4xl font-bold text-slate-800">Meus Tickets</h1>
-          { !user.is_superuser &&
-            <Link href="/support/send-ticket" className="hover:bg-blue-600 hover:text-white border-2 border-blue-600 text-blue-600 transition duration-200 flex items-center font-bold py-2 px-8 rounded focus:outline-none focus:shadow-outline">
-              Enviar Ticket
-            </Link>
-          }
-        </div>
-        { tickets.length === 0 ?
-          <p className="text-red-600 font-semibold text-lg mb-2">Você ainda não possui tickets cadastrados no momento.</p>
-          :
-          <div className="grid gap-8 lg:grid-cols-3">
-            {tickets.map(ticket => (
-              <div key={ticket.id} className="bg-white shadow-lg rounded-lg overflow-hidden hover:shadow-xl border-4 border-transparent hover:border-slate-200">
-                <Link href={`/support/${ticket.id}`} className="block px-4 py-5">
-                  <h2 className="text-lg font-medium text-gray-900 mb-2">{ticket.subject}</h2>
-                  <p className="text-md text-gray-700 mb-4">{ticket.content.slice(0, 30)}...</p>
-                  <div className="flex justify-between items-center">
-                    <p className="text-sm text-gray-600">{ticket.date}</p>
-                    <p className="text-sm text-gray-600">{ticket.cnpj}</p>
-                  </div>
-                </Link>
-              </div>
-            ))}
+        {isLoading ?
+          <div className="flex items-center justify-center mt-32">
+            <div className={`animate-spin ease-linear rounded-full h-32 w-32 pt-14 border-8 border-l-gray-400 border-r-gray-400 border-b-gray-400 border-t-indigo-900`} />
           </div>
+          :
+          <>
+            <div className="flex justify-between mb-8">
+              <h1 className="text-4xl font-bold text-slate-800">Meus Tickets</h1>
+              {!user.is_superuser &&
+                <Link href="/support/send-ticket" className="hover:bg-blue-600 hover:text-white border-2 border-blue-600 text-blue-600 transition duration-200 flex items-center font-bold py-2 px-8 rounded focus:outline-none focus:shadow-outline">
+                  Enviar Ticket
+                </Link>
+              }
+            </div>
+            {tickets.length === 0 ?
+              <p className="text-red-600 font-semibold text-lg mb-2">Você ainda não possui tickets cadastrados no momento.</p>
+              :
+              <div className="grid gap-8 lg:grid-cols-3">
+                {tickets.map(ticket => (
+                  <div key={ticket.id} className="bg-white shadow-lg rounded-lg overflow-hidden hover:shadow-xl border-4 border-transparent hover:border-slate-200">
+                    <Link href={`/support/${ticket.id}`} className="block px-4 py-5">
+                      <h2 className="text-lg font-medium text-gray-900 mb-2">{ticket.subject}</h2>
+                      <p className="text-md text-gray-700 mb-4">{ticket.content.slice(0, 30)}...</p>
+                      <div className="flex justify-between items-center">
+                        <p className="text-sm text-gray-600">{ticket.date}</p>
+                        <p className="text-sm text-gray-600">{ticket.cnpj}</p>
+                      </div>
+                    </Link>
+                  </div>
+                ))}
+              </div>
+            }
+          </>
         }
       </BaseLayout>
     </ProtectedRoute>
